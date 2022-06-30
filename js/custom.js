a182cbf773c29bfc390080b0b55d84c8924e557d
Annie Kyles <anniek@federation.net.nz>
2022-07-01 11:00:38 +1200

    Animate first section pattern

--- js/custom.js
+++ js/custom.js
@@ -3,3 +3,36 @@ document.addEventListener('click', function (event) {
 	if (!event.target.matches('#arrow-down')) return;
 	document.querySelector('section').scrollIntoView({behavior: 'smooth'});
 }, false);
+
+// Move pattern on scroll
+const scrollElement = document.querySelector(".pattern");
+const scrollOffset = 100;
+ 
+const elementInView = (el, offset = 0) => {
+  const elementTop = el.getBoundingClientRect().top;
+ 
+  return (
+    elementTop <= 
+    ((window.innerHeight || document.documentElement.clientHeight) - offset)
+  );
+};
+ 
+const displayScrollElement = () => {
+  scrollElement.classList.add('scrolled');
+}
+ 
+const hideScrollElement = () => {
+  scrollElement.classList.remove('scrolled');
+}
+ 
+const handleScrollAnimation = () => {
+  if (elementInView(scrollElement, scrollOffset)) {
+      displayScrollElement();
+  } else {
+    hideScrollElement();
+  }
+}
+ 
+window.addEventListener('scroll', () => {
+  handleScrollAnimation();
+})
