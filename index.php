773910e65ca2adb61d4792bb9fd507f5fbdf56c0
Annie Kyles <anniek@federation.net.nz>
2022-07-01 14:38:18 +1200

    JC's copy changes

--- index.php
+++ index.php
@@ -22,7 +22,7 @@
         <section>
             <div class="content">
                 <h1>Kia ora</h1>
-                <p class="intro"><strong>FEDERATION</strong> offer a high level of website expertise, drawing on vast knowledge in the website design and development space. No brief is the same, but we ensure the final product is needs driven, working to best practise principles to ensure each project caters to unique key objectives.</p>
+                <p class="intro"><strong>FEDERATION</strong> offer a high level of website expertise, drawing on vast knowledge in the website design and development space. No brief is the same, but we ensure the final product is needs driven, working to best practice principles to ensure each project caters to unique key objectives.</p>
                 <p class="intro">We’ve designed and developed multiple websites ranging from regional development based platforms right through to high end solutions with e-commerce functionality. While many aspects will vary across web projects, we believe your website is the heart of your brand presence and that the look and feel of your site needs to truly reflect your brand in the best and most accurate way possible.</p>
                 <p class="intro">Please take a closer look at some of our work below.</p>
             </div>            
@@ -33,7 +33,7 @@
                 <h2>The True Honey Co.</h2>
                 <p>The True Honey Co. is a fully New Zealand owned business whose passion is premium grade Mānuka honey. With their hives located in some of the most remote locations in New Zealand, The True Honey Co. make the world’s rarest and highest rated Mānuka honey.</p>
                 <p>A new ecommerce platform was required to address existing security issues, be seamlessly integrated into the European and Middle Eastern markets, and connect with different fulfilment, inventory, social and marketing backend systems. They also needed a new look and feel that aligned with their products premium status and exclusivity.</p>
-                <p>We explored a range of different ecommerce options that best suited TTHC immediate needs and their future goals and settled on Shopify. Looking at existing site analytics, user testing and developed personas we redesigned and built the site from the ground up allowing us to tell the True Honey story as well as creating seamless journeys from arrival to purchase and beyond. A photoshoot was also conducted to visually demonstrate the premium offering.</p>
+                <p>We explored a range of different ecommerce options that best suited TTHC. Considering their immediate needs and future goals we jointly settled on Shopify. Looking at the existing site analytics, user testing results and incorporating clear success metrics we redesigned and built the site from the ground up allowing us to tell the True Honey story as well as creating seamless journeys from arrival to purchase and beyond. A photoshoot was also conducted to visually demonstrate the premium offering.</p>
                 <a class="button" href="https://truehoney.co.nz/" target="_blank" title="The True Honey Co. website">Visit site</a>
             </div>            
         </section>
@@ -41,7 +41,7 @@
             <div class="content">
                 <img src="./images/auckland-invest.webp" alt="Auckland Invest - screenshot of website" title="Auckland Invest - screenshot of website">
                 <h2>Auckland Invest</h2>
-                <p>People are unlikely to invest if they haven’t visited or don’t know about a city or place. Auckland Invest wanted to increase the prominence of positive stories about doing business in Auckland by show people that Auckland is young and vibrant rather than telling them. This would fall in line with the wider 2021 "Opportunity is Calling" campaign.</p>
+                <p>People are unlikely to invest if they haven’t visited or don’t know about a city or place. Auckland Invest wanted to increase the prominence of positive stories about doing business in Auckland by showing people that Auckland is young and vibrant rather than just telling them. This would fall in line with the wider 2021 "Opportunity is Calling" campaign.</p>
                 <p>A new website was developed to help tell these stories, remove pain points from their existing website, and quickly curate content and contacts based on individual users preferences. Working with the team from AI we considered current analytics and user testing data to help create new tools and user journeys. A new look and feel including bespoke photography was implemented to help tell stories of a confident, modern and vibrant city.</p>
                 <p>A homepage filter system empowered users to find information that was tailored to their area of interest and quickly put them in contact with the most qualified person for their area.</p>
                 <a class="button" href="https://www.aucklandnz.com/invest" target="_blank" title="Auckland Invest website">Visit site</a>
@@ -57,7 +57,7 @@
                     <li>Feel current while also market leading from a design perspective</li>
                     <li>Better reflect the ‘Better business, together’ proposition</li>
                 </ul>
-                <p>To portray the "Better Business, Together" brand proposition we suggested an interactive homepage which serves a user with a custom selection of content based on their reason for visiting the site - as captured in a drop down form at the top of the homepage. This allowed Orbit to put their best foot forward when assisting every site visitor.</p>
+                <p>To portray the "Better Business, Together" brand proposition we suggested an interactive homepage which serves the user with a custom selection of content based on their reason for visiting the site - as captured in a drop down form at the top of the homepage. This allowed Orbit to put their best foot forward when assisting every site visitor.</p>
                 <p>We used a rich approach to imagery to allow us to talk to the many facets of the Orbit business. A dynamic homepage cluster allowed us to showcase important aspects of the Orbit business while also keeping things fresh for returning website users by drawing a random mix of imagery from a curated set hosted behind the site.</p>
                 <p>With a large and complex business such as Orbit, we knew content would often need changing. News, Resources, and Our People were all sections that would require near constant updates. With this in mind, we suggested the Wordpress site be built through Beaver Builder to ensure ease of use for clients needing to execute content changes and create new pages.</p>
                 <p>This website is currently in a staging environment so unfortunately we're unable to share a live link at present.</p>
