a845d5ad67a5b0828e1b45f2289788532f1811b4
Annie Kyles <anniek@federation.net.nz>
2022-07-01 16:00:13 +1200

    Patterned section animations

--- js/custom.js
+++ js/custom.js
@@ -4,35 +4,12 @@ document.addEventListener('click', function (event) {
 	document.querySelector('section').scrollIntoView({behavior: 'smooth'});
 }, false);
 
-// Move pattern on scroll
-const scrollElement = document.querySelector(".pattern");
-const scrollOffset = 100;
- 
-const elementInView = (el, offset = 0) => {
-  const elementTop = el.getBoundingClientRect().top;
- 
-  return (
-    elementTop <= 
-    ((window.innerHeight || document.documentElement.clientHeight) - offset)
+// animate patterend sections on scroll into view
+const callback = (entries) => 
+  entries.forEach(entry => 
+    entry.isIntersecting && entry.target.classList.add('show')
   );
-};
- 
-const displayScrollElement = () => {
-  scrollElement.classList.add('scrolled');
-}
- 
-const hideScrollElement = () => {
-  scrollElement.classList.remove('scrolled');
-}
- 
-const handleScrollAnimation = () => {
-  if (elementInView(scrollElement, scrollOffset)) {
-      displayScrollElement();
-  } else {
-    hideScrollElement();
-  }
-}
- 
-window.addEventListener('scroll', () => {
-  handleScrollAnimation();
-})
+
+const observer = new IntersectionObserver(callback);
+const animate = document.querySelectorAll('.pattern');
+animate.forEach(div => observer.observe(div));
